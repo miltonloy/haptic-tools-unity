72cdf1b3d07db9cb544a27766d6bdc77a88ab45d
Milton Loyola <miltonloy@gmail.com>
2017-05-01 21:04:40 -0300

    detector de agarre de un objeto

---
+++ Assets/DetectoresPrueba.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity;
+
+public class DetectoresPrueba : MonoBehaviour {
+
+	private ProximityDetector _proximityDetector;
+	private GameObject _current;
+
+	// Use this for initialization
+	void Start () {
+		_proximityDetector = GetComponent<ProximityDetector>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Activado()
+	{
+		_current = _proximityDetector.CurrentObject;
+		// TODO avisarle a _current que esta siendo agarrado
+		// TODO con este esquema el script "handableObject" pierde la logica actual y queda para recibir la posicion de rotacion y movimiento segun el pinch
+
+	}
+
+	public void Desactivado()
+	{
+		// TODO avisarle a _current que se acabo
+	}
+}
---
+++ Assets/HapticTools/Scripts/Helpers/HandheldObject.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity;
+
+public class HandheldObject : MonoBehaviour {
+
+   public enum RotationMethod {
+      None,
+      Single,
+      Full
+    }
+
+    [SerializeField]
+    private PinchDetector _pinchDetectorA;
+    public PinchDetector PinchDetectorA {
+      get {
+        return _pinchDetectorA;
+      }
+      set {
+        _pinchDetectorA = value;
+      }
+    }
+
+    [SerializeField]
+    private PinchDetector _pinchDetectorB;
+    public PinchDetector PinchDetectorB {
+      get {
+        return _pinchDetectorB;
+      }
+      set {
+        _pinchDetectorB = value;
+      }
+    }
+
+    [SerializeField]
+    private RotationMethod _oneHandedRotationMethod;
+
+    private Transform _anchor;
+    private Rigidbody _rigidbody;
+    private bool _isActive = false;
+
+    private float _defaultNearClip;
+
+    void Start() {
+      // Acá va el anchor este que no se para que lo usa
+      GameObject pinchControl = new GameObject("Grab Anchor");
+      _anchor = pinchControl.transform;
+      _anchor.transform.parent = transform.parent;
+      transform.parent = _anchor;
+
+      _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+
+      bool didUpdate = false;
+      if(_pinchDetectorA != null)
+        didUpdate |= _pinchDetectorA.DidChangeFromLastFrame;
+      if(_pinchDetectorB != null)
+        didUpdate |= _pinchDetectorB.DidChangeFromLastFrame;
+
+      if (didUpdate) {
+        transform.SetParent(null, true);
+      }
+
+      if (_pinchDetectorA != null && _pinchDetectorA.IsActive) {
+        _isActive = true;
+        transformSingleAnchor(_pinchDetectorA);
+      } else if (_pinchDetectorB != null && _pinchDetectorB.IsActive) {
+        _isActive = true;
+        transformSingleAnchor(_pinchDetectorB);
+      } else {
+        _isActive = false;
+      }
+
+      _rigidbody.isKinematic = _isActive;
+
+      if (didUpdate) {
+        transform.SetParent(_anchor, true);
+      }
+    }
+
+    private void transformSingleAnchor(PinchDetector singlePinch) {
+      _anchor.position = singlePinch.Position;
+
+      switch (_oneHandedRotationMethod) {
+        case RotationMethod.None:
+          break;
+        case RotationMethod.Single:
+          Vector3 p = singlePinch.Rotation * Vector3.right;
+          p.y = _anchor.position.y;
+          _anchor.LookAt(p);
+          break;
+        case RotationMethod.Full:
+          _anchor.rotation = singlePinch.Rotation;
+          break;
+      }
+
+      _anchor.localScale = Vector3.one;
+    }
+}
