5fadc01c93d0b79e2984ad5f51be7dbff9cb1754
Milton Loyola <miltonloy@gmail.com>
2017-05-17 19:03:26 -0300

    Spawner de bombas. Bug detectado en intermitencia al tacto

--- Assets/HapticTools/Examples/Scripts/Spawner.cs
+++ Assets/HapticTools/Examples/Scripts/Spawner.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandheldSpawner : MonoBehaviour {
+public class Spawner : MonoBehaviour {
 
 	public GameObject spawneable;
 
 	public void Spawn ()
 	{
-		GameObject newSphere = GameObject.Instantiate(spawneable);
+		GameObject.Instantiate(spawneable);
 	}
 }
