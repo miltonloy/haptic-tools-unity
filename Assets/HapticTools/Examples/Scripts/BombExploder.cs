4d1976af34de4c9f9d72a79246c0a897b3393269
Milton Loyola <miltonloy@gmail.com>
2017-05-29 00:43:39 -0300

    Nuevos prefabs para prototipar rapido las pruebas de conceptos

---
+++ Assets/HapticTools/Examples/Scripts/BombExploder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombExploder : MonoBehaviour {
+
+	void Explode ()
+	{
+		Destroy(gameObject);
+	}
+}
